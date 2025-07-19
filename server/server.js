27f78b99792092eb560791cc2571c7e575377ef7
Patrick-mwanza <munyaopatrick002@gmail.com>
2025-07-19 16:47:18 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://recipesharinapp.netlify.app',
+    origin: '',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }
 });
